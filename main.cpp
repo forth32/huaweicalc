bc43ff9cc270848b36aa2c1e304b4766aa650426
forth32 <forth32@cad>
2014-11-15 12:55:32 +0300

    Добавлен ключ -r

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@
 int main(int argc, char* argv[]) {
 
 char codebuf[40];
+char imeibuf[16];
+char c;
 int opt;
 int ff=0; // флаг ключей команды
-
+int rf=0; // флаг reverse
+int i;
 
 // Если аргументов нет - запускаем графическую морду
 
@@ -21,7 +24,7 @@ if (argc == 1) {
 
 // аргументы есть - работаем в консольном режиме
 
-while ((opt = getopt(argc, argv, "fF123hH")) != -1) {
+while ((opt = getopt(argc, argv, "rRfF123hH")) != -1) {
   switch (opt) {
    case 'h':
    case 'H':
@@ -46,17 +49,31 @@ while ((opt = getopt(argc, argv, "fF123hH")) != -1) {
    case '3':
      ff=4;
      break;
+   case 'r':
+   case 'R':
+     rf=1;
+     break;
   }
 }   
 
-//printf("\n ff=%i  imei=%s",ff,argv[optind]);
 
 if (optind >= argc) {
   printf("\n Не указан IMEI\n\n");
   return 0;
 }
 
-if (strlen(argv[optind]) != 15) {
+strcpy(imeibuf,argv[optind]);
+
+// Разворот imei, если надо
+if (rf != 0) 
+  for (i=0;i<7;i++) {
+    c=imeibuf[i];
+    imeibuf[i]=imeibuf[14-i];
+    imeibuf[14-i]=c;
+  }
+
+
+if (strlen(imeibuf) != 15) {
   printf("\n Неправильная длина IMEI\n");
   return 0;
 }
@@ -65,16 +82,16 @@ switch (ff) {
   case 0: {
 
   // режим без ключей - вывдим форматирванный результат
-  encrypt_v1(argv[optind],codebuf,"e630upgrade");
+  encrypt_v1(imeibuf,codebuf,"e630upgrade");
   printf("\n Flash code        = %s",codebuf);
 
-  encrypt_v1(argv[optind],codebuf,"hwe620datacard");
+  encrypt_v1(imeibuf,codebuf,"hwe620datacard");
   printf("\n Encrypt_v1 code   = %s",codebuf);
 
-  calc2(argv[optind],codebuf);
+  calc2(imeibuf,codebuf);
   printf("\n Encrypt_v2 code   = %s",codebuf);
 
-  calc201(argv[optind],codebuf);
+  calc201(imeibuf,codebuf);
   printf("\n Encrypt_v201 code = %s\n\n",codebuf);
   return 0;
  }
@@ -82,22 +99,22 @@ switch (ff) {
 // режим с ключами - вывди только отдельные цифры
 
  case 1: {
-   encrypt_v1(argv[optind],codebuf,"e630upgrade");
+   encrypt_v1(imeibuf,codebuf,"e630upgrade");
    break;
  }
 
  case 2: {
-  encrypt_v1(argv[optind],codebuf,"hwe620datacard");
+  encrypt_v1(imeibuf,codebuf,"hwe620datacard");
   break;
 }
 
  case 3: {
-  calc2(argv[optind],codebuf);
+  calc2(imeibuf,codebuf);
   break;
 }
 
  case 4: {
-  calc201(argv[optind],codebuf);
+  calc201(imeibuf,codebuf);
   break;
 }
 }
